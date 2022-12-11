556c741649e4ddf1f3ed9231a44be7632ab58802
Eduardo Cortez <duduomena.ec@gmail.com>
2022-12-11 19:26:23 -0300

    Estudos

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,30 +1,117 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+})
 
-import './assets/main.css'
+
+
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
+
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
+
+       </div>  
+    
+    </div>
+   `,
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage:  'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            variantQuantity: 10     
+          },
+          {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+            variantQuantity: 0     
+          }
+        ],
+        cart: 0
+    }
+  },
+    methods: {
+      addToCart: function() {
+          this.cart += 1
+      },
+      updateProduct: function(index) {  
+          this.selectedVariant = index
+      }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product  
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+          if (this.premium) {
+            return "Free"
+          }
+            return 2.99
+        }
+    }
+})
 
 var app = new Vue({
     el: '#app',
     data: {
-      product: 'Socks',
-      description: 'A pair of warm fuzzy socks',
-      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-      inventory: 0,
-      onSale: false,
-      details: ["80% cotton","20% poliester", "Gender-neural"],
-      variants:
-     [
-       { 
-        variantId:2234,
-        variantColor: "green"
-       },
-       { variantId:2235,
-        variantColor: "blue"
-        }
-      ],
-      sizes: ["P","M","G","GG","XGG"],
-      cart: 0,
-    } 
-  })
-  
+      premium: true
+    }
+})
